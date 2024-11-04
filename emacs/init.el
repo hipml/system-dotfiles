e3232a42be7a9cae8f718667fba02fe602e8b4c8
Paul Lambert <paul.lambert@linux.com>
2024-11-03 20:22:44 -0600

    we're back on evil-mode. fixing treemacs/projectile integration

--- emacs/init.el
+++ emacs/init.el
@@ -95,7 +95,7 @@
   (setq evil-want-integration t)
 
   :config
-  (evil-mode 0))
+  (evil-mode 1))
 
 (use-package tree-sitter
   :ensure t
@@ -179,15 +179,12 @@
   :config
   (projectile-mode +1)
   (setq projectile-file-explorer 'treemacs)
-  ;;(setq projectile-enable-caching t)
-  ;; (setq projectile-switch-project-action 'treemacs)
   (setq projectile-project-root-files '(".git" ".hg" "package.json" "build.gradle"))
-  ;;(setq projectile-indexing-method 'native)
-  ;;:hook
-  ;;('find-file-hook . (lambda ()
-	;;				   (when (not (projectile-project-p))
-	;;					 (projectile-discover-project (buffer-file-name)))))
-  ;;('projectile-after-switch-project-hook 'treemacs)
+  (setq projectile-auto-discover t)
+  (setq projectile-project-search-path '("~/code/"))
+  (setq projectile-enable-caching t)
+  ;; not this deep yet (setq projectile-completion-system 'ivy)
+  (setq projectile-auto-update-cache t)
   :bind-keymap
   ("C-c p" . projectile-command-map))
 
@@ -199,12 +196,21 @@
   (treemacs-follow-mode t)
   (treemacs-filewatch-mode t)
   (treemacs-fringe-indicator-mode t)
-  ;; (setq treemacs-switch-project-action 'treemacs-add-and-switch)
-  )
+  (treemacs-git-mode 'deferred)
+  (treemacs-fringe-indicator-mode 'always)
+  :bind
+  (:map global-map
+		("M-0" . treemacs-select-window)
+		("C-x t 1" . treemacs-delete-other-windows)
+		("C-x t t" . treemacs)
+		("C-x t d" . treemacs-select-directory)))
 
 (use-package treemacs-projectile
   :after (treemacs projectile)
   :ensure t)
+
+(use-package treemacs-magit
+  :after (treemacs magit))
 
 (treemacs-start-on-boot)
 
@@ -235,7 +241,7 @@
  '(custom-safe-themes
    '("cd3a935a8ffa314b540e05877c97fc4651f62300f9f89d6e9e7ca822a4d591f2" "c0fe46c2c91bda132c98f1f882a83ee263335a3c934d10f0db96c7dbccb7c8a0" "536622b90022666ba1ed1de27535fc79a8a2d0d03c8e7dd4a66872cb225e3bd9" "c30f1ac361bc0025b677e82de3b4a454f77b3abb6542278650e471dd80a6e36a" "9f96a5e589c9e5bfb299ea372ef82ae636f1a0b88b01bc3263d64cb0bfac4de4" "52526fdb0eafd76fdc1963a87a30bd38f70673407646ae13b72561b503dc6f69" "a4c78d5d55160c9a719a36724dba8e428958470dd7952ab0b7b715efd006f6f4" "8bf1e0be927767ae05d4035ee68f54998b112d548494676ec8d1d1b77e43c808" "1d8ed1460acd9d6352b46379ca6463e14b560ce659fb07ac1e808e19834ba798" default))
  '(package-selected-packages
-   '(treemacs-projectile treemacs auctex-cluttex auctex-cont-latexmk auctex-latexmk auctex-label-numbers markdown-mode pdf-tools auctex flycheck-ocaml merlin-eldoc merlin tuareg tree-sitter-langs tree-sitter treesitter magit evil elpy ein)))
+   '(treemacs-magit treemacs-projectile treemacs auctex-cluttex auctex-cont-latexmk auctex-latexmk auctex-label-numbers markdown-mode pdf-tools auctex flycheck-ocaml merlin-eldoc merlin tuareg tree-sitter-langs tree-sitter treesitter magit evil elpy ein)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
