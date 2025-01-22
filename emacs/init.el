80efa593ca99d473d92507f5b3ab82879ecd06de
Paul Lambert <paul.lambert@linux.com>
2025-01-22 09:21:19 -0600

    typo in JS-ts-mode config

--- emacs/init.el
+++ emacs/init.el
@@ -436,7 +436,7 @@
   (setq web-mode-enable-current-element-highlight t)) ; Highlight current element
 
 (use-package js
-  :ensure :nil
+  :ensure nil
   :mode (("\\.js\\'" . js-ts-mode)
          ("\\.jsx\\'" . js-ts-mode))
   :config
